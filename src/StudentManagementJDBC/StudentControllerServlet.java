8e313ec53ce67f1ebcc9015b13d12ffecb63498d
Manoj P <thedarkstrix@gmail.com>
2019-08-10 01:36:03 +0530

    Backend for Displaying Details from DB [1/2] ** Info will displayed at list-students.jsp

--- src/StudentManagementJDBC/StudentControllerServlet.java
+++ src/StudentManagementJDBC/StudentControllerServlet.java
@@ -1,14 +1,17 @@
 package StudentManagementJDBC;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+
 
 /**
  * Servlet implementation class StudentControllerServlet
@@ -40,6 +43,15 @@ private StudentDBUtil studentDBUtil;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	private void listStudents(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		List<Student> students = studentDBUtil.getStudents();
+		
+		request.setAttribute("STUDENT_LIST", students);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/list-students.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
--- src/StudentManagementJDBC/StudentDBUtil.java
+++ src/StudentManagementJDBC/StudentDBUtil.java
@@ -1,11 +1,64 @@
 package StudentManagementJDBC;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sql.DataSource;
 
 public class StudentDBUtil {
+	private DataSource dataSource;
 
-	public StudentDBUtil(DataSource dataSource) {
-		// TODO Auto-generated constructor stub
+	public StudentDBUtil(DataSource theDataSource) {
+		dataSource = theDataSource;
 	}
 
+
+	public List<Student> getStudents() throws Exception{
+		List<Student> students = new ArrayList<>();
+		Connection myConn = null;
+		java.sql.Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+		myConn = dataSource.getConnection();
+		String sql = "select * from student order by last_name";
+		
+		myStmt = myConn.createStatement();
+		
+		myRs = myStmt.executeQuery(sql);
+		
+		while(myRs.next()) {
+			int id = myRs.getInt("id");
+			String firstName = myRs.getString("first_name");
+			String lastName = myRs.getString("last_name");
+			String email = myRs.getString("email");
+			
+			Student tempStudent = new Student(id,firstName,lastName,email);
+			
+			students.add(tempStudent);
+		}
+			return students;
+		}finally {
+			close(myConn,myStmt,myRs);
+		}
+		
+	}
+	private void close(Connection myConn, Statement myStmt, ResultSet myRs) {
+		try {
+			if(myRs != null) {
+				myRs.close();
+			}
+			if(myStmt != null) {
+				myStmt.close();
+			}
+			if(myConn != null) {
+				myConn.close();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
